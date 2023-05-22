989709092979e2a239efcd96d15289d541f547de
Anna <anna@Nareks-iMac.local>
2023-05-22 13:22:48 +0400

    first commit

---
+++ src/js/scripts.js
@@ -0,0 +1,12 @@
+// Custom Scripts
+// @@include('main.js')
+@@include('./libs/jquery-3.6.0.js')
+@@include('./libs/swiper.js')
+@@include('./libs/aos.js')
+AOS.init({
+    once:true,
+    duration: 800,
+    // disable: 'mobile'
+});
+
+
