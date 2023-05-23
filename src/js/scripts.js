637dd3d4406980c3c5379514d589cf5712affe4f
Anna <anna@Nareks-iMac.local>
2023-05-23 15:09:40 +0400

    youtubeSlider

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,7 @@
 // Custom Scripts
 // @@include('main.js')
 @@include('./libs/swiper.js')
-var homeSwiper = new Swiper(".home-slider__inner .homeSwiper", {
+const homeSwiper = new Swiper(".home-slider__inner .homeSwiper", {
     loop: true,
     navigation: {
         nextEl: ".home-slider__inner .swiper-button-next",
@@ -12,6 +12,18 @@ var homeSwiper = new Swiper(".home-slider__inner .homeSwiper", {
     },
 });
 
+const youtubeSwiper = new Swiper(".youtube-slider__inner .youtubeSwiper", {
+    // loop: true,
+    slidesPerView: 4,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: ".youtube-slider__inner .swiper-button-next",
+        prevEl: ".youtube-slider__inner .swiper-button-prev",
+    },
+    pagination: {
+        el: ".youtube-slider__inner .swiper-pagination",
+    },
+})
 
 const homePagination = document.querySelectorAll('.home-slider__inner .swiper-pagination-bullet');
 for (let i = 0; i < homePagination.length; i++) {
