88b78f90938056b5a4542b7a3e5cced908aa9d03
Anna <anna@Nareks-iMac.local>
2023-05-24 17:05:10 +0400

    add hovers

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,7 @@
 // Custom Scripts
 // @@include('main.js')
 @@include('./libs/swiper.js')
+@@include('./libs/jquery-3.6.0.js')
 const homeSwiper = new Swiper(".home-slider__inner .homeSwiper", {
     loop: true,
     navigation: {
@@ -29,3 +30,28 @@ const homePagination = document.querySelectorAll('.home-slider__inner .swiper-pa
 for (let i = 0; i < homePagination.length; i++) {
     homePagination[i].addEventListener('click', () => homeSwiper.slideTo(i))
 }
+
+const youtubePagination = document.querySelectorAll('.youtube-slider__inner .swiper-pagination-bullet');
+for (let i = 0; i < youtubePagination.length; i++) {
+    youtubePagination[i].addEventListener('click', () => youtubeSwiper.slideTo(i))
+}
+$(document).ready(function () {
+    $('.header-call__button-phone .header-call__button-inner').on('click', function () {
+        const el = $(this).parent().find('.header-call__button-content')
+        el.toggle()
+    })
+    $('.account__tabs-item').on('click', function () {
+        const el = $(this).parent().find('.account__tabs-item')
+        const content = $('.account__tabs-content-item')
+        el.removeClass('active')
+        $(this).addClass('active')
+        content.hide()
+        content.eq($(this).index()).show()
+    })
+    $('.header-actions__item-inner').on('click', function () {
+        $(this).toggleClass('active')
+        const el = $(this).parent().find('.header-actions__item-content')
+        el.toggle()
+    })
+})
+
