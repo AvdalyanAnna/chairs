8a838d3493283b1d62b56f6496443739a05a0f9e
Anna Avdalyan <anna_avdalyan@intdevels.com>
2023-06-29 14:02:46 +0400

    fix

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -50,15 +50,14 @@ $(document).ready(function () {
         $('.products-list__grid').addClass($(this).data('columns'))
     })
 })
-
 $(document).ready(function () {
 
-    function $slider(elClass,params ){
-       return  new Swiper(elClass, {
+    function $slider(elClass, params) {
+        return new Swiper(elClass, {
             loop: true,
             slidesPerView: params.slidesPerView || 4,
             spaceBetween: params.spaceBetween || 0,
-           centeredSlides:params.centeredSlides || false,
+            centeredSlides: params.centeredSlides || false,
             navigation: {
                 nextEl: `${elClass} .swiper-button-next`,
                 prevEl: `${elClass} .swiper-button-prev`,
@@ -66,12 +65,22 @@ $(document).ready(function () {
         })
 
     }
-    const productMin = $slider('.products-min__slider-inner',{slidesPerView:'auto',spaceBetween:8})
-    // const productMin1 = $slider('.products-list__grid-4 .products-min__slider-inner',{slidesPerView:auto,spaceBetween:8})
-    const productBig = $slider('.products-big__slider-inner',{slidesPerView:1,spaceBetween:0})
-    const productNewSwiper = $slider('.productNewSwiper',{})
-    console.log({productMin,productBig,productNewSwiper})
 
+    const productMin = $slider('.products-min__slider-inner', {
+        slidesPerView: 'auto',
+        spaceBetween: 8
+    })
+    const productBig = $slider('.products-big__slider-inner', {
+        slidesPerView: 1,
+        spaceBetween: 0
+    })
+    const productNewSwiper = $slider('.productNewSwiper', {})
+    console.log({productMin, productBig, productNewSwiper})
+    $('.products-min__slider-inner .swiper-slide').on('click', function () {
+        const data = $(this).data('swiper-slide-index')
+        const parentIndex = $(this).parents('.products-item').data('index') || 0
+        productBig[parentIndex].slideTo(data + 1)
+    })
 })
 
 jQuery(document).ready(function ($) {
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,12 +1,11 @@
-
 $(document).ready(function () {
 
-    function $slider(elClass,params ){
-       return  new Swiper(elClass, {
+    function $slider(elClass, params) {
+        return new Swiper(elClass, {
             loop: true,
             slidesPerView: params.slidesPerView || 4,
             spaceBetween: params.spaceBetween || 0,
-           centeredSlides:params.centeredSlides || false,
+            centeredSlides: params.centeredSlides || false,
             navigation: {
                 nextEl: `${elClass} .swiper-button-next`,
                 prevEl: `${elClass} .swiper-button-prev`,
@@ -14,10 +13,20 @@ $(document).ready(function () {
         })
 
     }
-    const productMin = $slider('.products-min__slider-inner',{slidesPerView:'auto',spaceBetween:8})
-    // const productMin1 = $slider('.products-list__grid-4 .products-min__slider-inner',{slidesPerView:auto,spaceBetween:8})
-    const productBig = $slider('.products-big__slider-inner',{slidesPerView:1,spaceBetween:0})
-    const productNewSwiper = $slider('.productNewSwiper',{})
-    console.log({productMin,productBig,productNewSwiper})
 
+    const productMin = $slider('.products-min__slider-inner', {
+        slidesPerView: 'auto',
+        spaceBetween: 8
+    })
+    const productBig = $slider('.products-big__slider-inner', {
+        slidesPerView: 1,
+        spaceBetween: 0
+    })
+    const productNewSwiper = $slider('.productNewSwiper', {})
+    console.log({productMin, productBig, productNewSwiper})
+    $('.products-min__slider-inner .swiper-slide').on('click', function () {
+        const data = $(this).data('swiper-slide-index')
+        const parentIndex = $(this).parents('.products-item').data('index') || 0
+        productBig[parentIndex].slideTo(data + 1)
+    })
 })
