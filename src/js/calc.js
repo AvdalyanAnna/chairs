0d24cfa23ad9a5cfce16009846a0ccd8f0dd1075
Anna Avdalyan <anna_avdalyan@intdevels.com>
2023-07-23 01:12:14 +0400

    fix

--- src/js/calc.js
+++ src/js/calc.js
@@ -78,7 +78,7 @@ $(document).ready(function () {
         $('.modal-product .product-tab__item').removeClass('product-tab__item--active')
         $(`.modal-product .product-tab__item[data-tab="${$(this).data('tab')}"]`).addClass('product-tab__item--active')
     })
-    $('.product-single__body-right__price-two-send').on('click', function (){
+    $('.product-single__body-right__price-two-send').on('click', function () {
         let flag = false
         elements.each(index => {
             let data = elements.eq(index).data('color')
@@ -88,29 +88,75 @@ $(document).ready(function () {
             $('.product-single__body-right__price-two-send').text('ПЕРЕЙТИ В КОРЗИНУ')
         }
     })
-    $('.modal-product__colors-menu-item').on('click', function (){
+    $('.modal-product__colors-menu-item').on('click', function () {
         $('.modal-product__colors-menu-item').removeClass('active')
         $(this).addClass('active')
         const tab = $(this).data('tab')
-        if(tab === 'all'){
+        if (tab === 'all') {
             $(this).parents('.product-tab').find('.modal-product__color').show()
-        }else{
+        } else {
             $(this).parents('.product-tab').find('.modal-product__color').hide()
             $(this).parents(`.product-tab`).find(`.modal-product__color[data-tab="${tab}"]`).show()
         }
     })
 
-    $('.change-star').on('click', function (e){
+    $('.change-star').on('click', function (e) {
         e.preventDefault()
-       const star =  $(this).data('star')
+        const star = $(this).data('star')
         const els = $(this).parent().find('.change-star')
-        els.each(i=>{
+        els.each(i => {
             let img = '/img/icon/star-active.svg'
-            if(+star  >= i + 1)  img = '/img/icon/star-active.svg'
+            if (+star >= i + 1) img = '/img/icon/star-active.svg'
             else img = '/img/icon/star.svg'
             els.eq(i).html(
                 `<img src="${img}" alt="star" />`
             )
         })
     })
+    $('.modal-review-step__form-item_photo').on('change', function (e) {
+        let files = e.target.files; // Get the selected file
+        for (let i = 0;i<files.length;i++){
+            if (files[i]) {
+                console.log(e.target.result)
+                let reader = new FileReader(); // Create a FileReader object
+                reader.onload = function (event) {
+                    let imageSrc = event.target.result; // Get the data URL for the current file
+                    showImagePreview(imageSrc, files[i]); // Call the function to show the image preview
+                };
+
+                reader.readAsDataURL(files[i]); // Read the file as data URL
+            } else {
+                // If no file is selected, reset the image src to a placeholder or hide it
+            }
+        }
+
+
+        // Check if a file was selected
+
+    })
+
+    function showImagePreview(src,file) {
+        $('.modal-review-step__form-item-label-1 .photos').hide();
+        $('.modal-review-step__form-item-label-2').show();
+        let previewId = Date.now();
+        const html = `
+      <div id="preview_${previewId}"  class="modal-review-step__form-item-label-img">
+        <img src="${src}" alt="files" />
+        <div class="modal-review-step__form-item-label-img-close" data-preview-id="${previewId}">
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M8.94002 8.00001L11.8067 5.14001C11.9322 5.01448 12.0027 4.84421 12.0027 4.66668C12.0027 4.48914 11.9322 4.31888 11.8067 4.19335C11.6811 4.06781 11.5109 3.99728 11.3334 3.99728C11.1558 3.99728 10.9856 4.06781 10.86 4.19335L8.00002 7.06001L5.14002 4.19335C5.01448 4.06781 4.84422 3.99728 4.66669 3.99728C4.48915 3.99728 4.31889 4.06781 4.19335 4.19335C4.06782 4.31888 3.99729 4.48914 3.99729 4.66668C3.99729 4.84421 4.06782 5.01448 4.19335 5.14001L7.06002 8.00001L4.19335 10.86C4.13087 10.922 4.08127 10.9957 4.04743 11.077C4.01358 11.1582 3.99615 11.2453 3.99615 11.3333C3.99615 11.4214 4.01358 11.5085 4.04743 11.5897C4.08127 11.671 4.13087 11.7447 4.19335 11.8067C4.25533 11.8692 4.32906 11.9188 4.4103 11.9526C4.49154 11.9865 4.57868 12.0039 4.66669 12.0039C4.75469 12.0039 4.84183 11.9865 4.92307 11.9526C5.00431 11.9188 5.07805 11.8692 5.14002 11.8067L8.00002 8.94001L10.86 11.8067C10.922 11.8692 10.9957 11.9188 11.077 11.9526C11.1582 11.9865 11.2453 12.0039 11.3334 12.0039C11.4214 12.0039 11.5085 11.9865 11.5897 11.9526C11.671 11.9188 11.7447 11.8692 11.8067 11.8067C11.8692 11.7447 11.9188 11.671 11.9526 11.5897C11.9865 11.5085 12.0039 11.4214 12.0039 11.3333C12.0039 11.2453 11.9865 11.1582 11.9526 11.077C11.9188 10.9957 11.8692 10.922 11.8067 10.86L8.94002 8.00001Z" fill="white"/>
+            </svg>
+        </div>
+      </div>
+    `;
+
+        $('.modal-review-step__form-item-label-2-list').prepend(html);
+        $('.modal-review-step__form-item-label-img-close').on('click',function (){
+            let previewIdToDelete = $(this).data('preview-id');
+            $(`#preview_${previewIdToDelete}`).remove();
+            let remainingFiles = Array.from($('.modal-review-step__form-item_photo-input').files).filter((f) => f !== file);
+            $('.modal-review-step__form-item_photo-input').prop('files', new FileList(remainingFiles));
+        })
+    }
+
 })
--- src/js/review.js
+++ src/js/review.js
@@ -6,7 +6,66 @@ $(function () {
             $('.modal').hide()
             return false
         }
+        if(next === 'modal-review-step-2'){
+            const el = $(this).parents('.modal-review-step').find('textarea')
+            let flag = true
+            $('.modal-review-step__form-item').removeClass('error-block')
+            $('.modal-review-step__form-item .is-error').remove()
+            el.each(i=>{
+                const elVal = el.eq(i)
+                if(elVal.val().length === 0){
+                    flag = false
+                    elVal.parents('.modal-review-step__form-item').append(`
+                        <div class="is-error">Не заполнено</div>
+                    `)
+                    elVal.parents('.modal-review-step__form-item').addClass('error-block')
+                }
+            })
+            if(flag) {
+                $('.modal-review-step').hide()
+                $(`.${next}`).show()
+            }else{
+                return false
+            }
+            return flag
+        }
+        if(next === 'modal-review-step-3'){
+            const el = $(this).parents('.modal-review-step').find('input[type="text"]')
+
+            let flag1 = true
+            $('.modal-review-step__form-item').removeClass('error-block')
+            $('.modal-review-step__form-item .is-error').remove()
+            el.each(i=>{
+                const elVal = el.eq(i)
+                if(elVal.val().length === 0){
+                    flag1 = false
+                    elVal.parents('.modal-review-step__form-item').append(`
+                        <div class="is-error">Не заполнено</div>
+                    `)
+                    elVal.parents('.modal-review-step__form-item').addClass('error-block')
+                } else if(!isValidEmail(elVal.val())){
+                    flag1 = false
+                    elVal.parents('.modal-review-step__form-item').append(`
+                        <div class="is-error">Не e-mail</div>
+                    `)
+                    elVal.parents('.modal-review-step__form-item').addClass('error-block')
+                }
+            })
+            if(flag1) {
+                $('.modal-review-step').hide()
+                $(`.${next}`).show()
+            }else{
+                return false
+            }
+            return flag1
+        }
         $('.modal-review-step').hide()
         $(`.${next}`).show()
     })
+
+    function isValidEmail(email) {
+        let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        console.log(emailPattern.test(email))
+        return emailPattern.test(email);
+    }
 })
