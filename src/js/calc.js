facdba67bda171d29df436bab0d6677b8eee94c2
Anna Avdalyan <anna_avdalyan@intdevels.com>
2023-08-10 01:42:22 +0400

    fix

--- src/js/calc.js
+++ src/js/calc.js
@@ -321,4 +321,11 @@ $(document).ready(function () {
         });
         divElements.appendTo(".catalogue__table tbody");
     }
+
+    $('.supplier__label--file input').on('change', function (e){
+        const parent = $(this).parents('.supplier__label--file');
+        const fileName = e.target.files[0].name;
+        parent.find('.supplier__label-content').html(fileName);
+    });
+
 })
